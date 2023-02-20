3a52796252ce8fef7cc4be01f347b2fa9fd546c1
Papuna Maglaperidze <papunamaglaperidze@Papunas-MacBook-Pro.local>
2023-02-20 14:57:10 -0800

    made several changes

--- main.js
+++ main.js
@@ -1,10 +1,61 @@
 const sliderButton = document.querySelector('.round');
+const slider = document.querySelector('.slider');
+
+
+// Switch button Monthly to Yearly
 
 sliderButton.addEventListener('click', (e) => {
+    var price = document.getElementById('price');
     sliderButton.classList.toggle('round_on');
-    if (sliderButton.class == "round") {
-        sliderButton.class = "round_on";
-    } else if (sliderButton.includes('round_on')) {
-      sliderButton.classList.remove('round_on')  
+    if (sliderButton.classList == 'round round_on') {
+        price.innerHTML = "$36.00";
+    } else {
+        price.innerHTML = "$16.00";
     }
-} );
+
+});
+
+
+slider.addEventListener('click', (y) => {
+    var views = document.getElementById('views');
+    if (slider.value < 50) {
+        views.innerHTML = "30K PAGEVIEWS"
+    } else if (slider.value > 50) {
+        views.innerHTML = "70K PAGEVIEWS"
+    }
+});
+
+
+
+
+
+
+// - 10K pageviews / $8 per month
+// - 50K pageviews / $12 per month
+// - 100K pageviews / $16 per month
+// - 500k pageviews / $24 per month
+// - 1M pageviews / $36 per month
+
+
+var rates = [
+    { 
+        pageviews: "10k pageviews",
+        permonth: "$8 per month"
+    },
+    { 
+        pageviews: "50k pageviews",
+        permonth: "$12 per month"
+    },
+    { 
+        pageviews: "100k pageviews",
+        permonth: "$16 per month"
+    },
+    { 
+        pageviews: "500k pageviews",
+        permonth: "$24 per month"
+    },
+    { 
+        pageviews: "1M pageviews",
+        permonth: "$36 per month"
+    }
+]
