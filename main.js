4bb47c7e9d92efebf76421706777defac059d388
Papuna Maglaperidze <papunamaglaperidze@Papunas-MBP.lan>
2023-02-22 01:35:56 -0800

    final touches made to the files

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const sliderButton = document.querySelector('.round');
 const slider = document.querySelector('.slider');
 
@@ -8,34 +10,72 @@ sliderButton.addEventListener('click', (e) => {
     var price = document.getElementById('price');
     sliderButton.classList.toggle('round_on');
     if (sliderButton.classList == 'round round_on') {
-        price.innerHTML = "$36.00";
+        calculateYearly();
     } else {
-        price.innerHTML = "$16.00";
+        changeValues(),
+        document.getElementById('month').innerHTML = '/ monthly'
     }
 
 });
 
+// Discount Changes
+
+slider.addEventListener('change', (values) => {
+
+    if (sliderButton.classList == 'round round_on') {
+        calculateYearly();
+    } else {
+        changeValues();
+    }
+
+})
 
 // Changes the value of HTML value
 
-slider.addEventListener('change', (y) => {
-    var views = document.getElementById('views');
-    var price = document.getElementById('price');
+function changeValues() {
     if (slider.value == 0) {
         views.innerHTML = "10K PAGEVIEWS",
         price.innerHTML = "$8.00"
-    } else if (slider.value == 25) {
+    }   else if (slider.value == 25) {
         views.innerHTML = "50K PAGEVIEWS",
         price.innerHTML = "$12.00"
     } else if (slider.value == 50) {
         views.innerHTML = "100K PAGEVIEWS",
         price.innerHTML = "$16.00"
-    }  
-    else if (slider.value == 75)  {
+    }  else if (slider.value == 75)  {
         views.innerHTML = "500K PAGEVIEWS",
         price.innerHTML = "$24.00"
     } else if (slider.value == 100) {
         views.innerHTML = "1M PAGEVIEWS",
         price.innerHTML = "$36.00"
-    }    
-});
+    } 
+}
+
+// Changes the value of Yearly HTML
+
+function calculateYearly() {
+    if (slider.value == 0) {
+        views.innerHTML = "10K PAGEVIEWS",
+        price.innerHTML = "$24.00"
+        document.getElementById('month').innerHTML = '/ yearly'
+    }   else if (slider.value == 25) {
+        views.innerHTML = "50K PAGEVIEWS",
+        price.innerHTML = "$36.00"
+        document.getElementById('month').innerHTML = '/ yearly'
+    } else if (slider.value == 50) {
+        views.innerHTML = "100K PAGEVIEWS",
+        price.innerHTML = "$48.00"
+        document.getElementById('month').innerHTML = '/ yearly'
+    }  else if (slider.value == 75)  {
+        views.innerHTML = "500K PAGEVIEWS",
+        price.innerHTML = "$72.00"
+        document.getElementById('month').innerHTML = '/ yearly'
+    } else if (slider.value == 100) {
+        views.innerHTML = "1M PAGEVIEWS",
+        price.innerHTML = "$108.00"
+        document.getElementById('month').innerHTML = '/ yearly'
+    } 
+}
+
+
+
